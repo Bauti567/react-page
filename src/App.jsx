bb10b755075ee531611d15134411ed4338a19865
Juan Bautista <jbautistapulido@gmail.com>
2024-01-06 19:04:07 -0500

    added Features

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { GlobalProvider } from './context/GlobalState';
+import Header from './components/Header'
+import Balance from './components/Balance';
+import TransactionForm from './components/TransactionForm';
+
 
 function App(){
   return (
-    <div>Hola mundo </div>
+    <GlobalProvider>
+      <Header></Header>
+      <Balance></Balance>
+      <TransactionForm></TransactionForm>
+      <h1>Hola mundo</h1>
+    </GlobalProvider>
   )
 }
 
---
+++ src/context/GlobalState.jsx
@@ -0,0 +1,23 @@
+import { createContext,useContext, useState } from "react";
+
+export const Context = createContext()
+
+export const useGlobalState = ()=>{
+    const context = useContext(Context)
+    return context
+}
+
+
+export const GlobalProvider = ({children}) =>{
+
+    const [state, setState] = useState([])
+
+    return (
+        <Context.Provider value={{
+            state, setState,
+        }}>
+            {children}
+        </Context.Provider>
+    )
+}
+
